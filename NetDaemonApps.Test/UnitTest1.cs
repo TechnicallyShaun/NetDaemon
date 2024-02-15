219b289af34e69e3f9364cffadf5fef6cf8bcc2f
Smithy <121695494+ssmithy@users.noreply.github.com>
2024-02-15 21:17:40 +0000

    Test

--- NetDaemonApps.Test/UnitTest1.cs
+++ NetDaemonApps.Test/UnitTest1.cs
@@ -12,5 +12,11 @@ namespace NetDaemonApps.Test
         {
             Assert.Pass();
         }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Pass();
+        }
     }
 }
