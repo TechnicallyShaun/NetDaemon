022553ba69d6b314b3d291da4819fdeb32e3b6c5
Smithy <121695494+ssmithy@users.noreply.github.com>
2024-02-18 22:40:10 +0000

    #9 Add TestBase to simplify creating app tests

--- NetDaemonApps.Test/Apps/SuccessfullyLoadedNotifierAppTests.cs
+++ NetDaemonApps.Test/Apps/SuccessfullyLoadedNotifierAppTests.cs
@@ -1,33 +1,42 @@
 ﻿using NetDaemonApps.Dtos;
+using System.Linq.Expressions;
 
 namespace NetDaemonApps.Test.Apps;
 
-public class SuccessfullyLoadedNotifierAppTests
+public class SuccessfullyLoadedNotifierAppTests : AppTestsBase<SuccessfullyLoadedNotifierApp>
 {
     [Test]
     public void Init_CallsNotify()
     {
-        var mock = new Mock<IHaContext>();
+        //Arrange
+        var mock = FakeHass();
+        var expected = StubNotify();
+        mock.Setup(h =>
+                h.CallService(
+                    HaNotify.Domain, HaNotify.Service,
+                    It.IsAny<ServiceTarget>(),
+                    It.Is(TheSameAs(expected))))
+            .Verifiable(Times.Once());
+
+        //Act
         var helloWorld = new SuccessfullyLoadedNotifierApp(mock.Object);
-        var expected = new HaNotify(SuccessfullyLoadedNotifierApp.Title, SuccessfullyLoadedNotifierApp.Message);
 
-        mock.Verify(x => x.CallService(HaNotify.Domain, HaNotify.Service, It.IsAny<ServiceTarget>(), It.Is<HaNotify>(y => y.Equals(expected))), Times.Once());
+        //Assert
+        mock.VerifyAll();
     }
+
+    private Expression<Func<HaNotify, bool>> TheSameAs(HaNotify expected) =>
+        x => IsMatch(x, expected);
+    private static bool IsMatch(HaNotify a, HaNotify b)
+        => a.title == b.title && a.message == b.message;
+
+    /// <summary>
+    /// Ensure the matching method works correctly
+    /// </summary>
     [Test]
-    public void TestHashes()
-    {
-        var expected = new HaNotify(SuccessfullyLoadedNotifierApp.Title, SuccessfullyLoadedNotifierApp.Message);
-        var actual = new HaNotify(SuccessfullyLoadedNotifierApp.Title, SuccessfullyLoadedNotifierApp.Message);
+    public void Matching_Notify_HappyPath() =>
+        IsMatch(StubNotify(), StubNotify());
 
-        var eHash = expected.GetHashCode();
-        var aHash = actual.GetHashCode();
-
-        eHash.Equals(aHash).Should().BeTrue();
-    }
-
-    [Test]
-    public void TestAttribute()
-    {
-        Attribute.GetCustomAttribute(typeof(SuccessfullyLoadedNotifierApp), typeof(NetDaemonAppAttribute)).Should().NotBeNull();
-    }
+    private static HaNotify StubNotify() => new(SuccessfullyLoadedNotifierApp.Title, SuccessfullyLoadedNotifierApp.Message);
+    protected override SuccessfullyLoadedNotifierApp CreateSUT(IHaContext hass) => new(hass);
 }
--- NetDaemonApps/Dtos/HaNotify.cs
+++ NetDaemonApps/Dtos/HaNotify.cs
@@ -6,7 +6,4 @@ public class HaNotify(string _title, string _message)
     public static string Service => "persistent_notification";
     public string title => _title;
     public string message => _message;
-
-    public override int GetHashCode() => title.GetHashCode() + message.GetHashCode();
-    public override bool Equals(object? obj) => GetHashCode() == obj?.GetHashCode();
 }
