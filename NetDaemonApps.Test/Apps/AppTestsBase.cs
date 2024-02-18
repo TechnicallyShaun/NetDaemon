b1e1a6e3a6a33909e7074b30411d0124ec3e41de
Smithy <121695494+ssmithy@users.noreply.github.com>
2024-02-18 22:43:32 +0000

    Tech/9/refactor improve test (#15)
    * #9 Extract bulked classes
    
    * #9 Add TestBase to simplify creating app tests

---
+++ NetDaemonApps.Test/Apps/AppTestsBase.cs
@@ -0,0 +1,29 @@
+﻿namespace NetDaemonApps.Test.Apps;
+
+public abstract class AppTestsBase<T>
+    where T : class
+{
+    /// <summary>
+    /// Ensure the App Class has the `NetDaemonAppAttribute` attribute.
+    /// (This is required for NetDaemon to correctly load the app)
+    /// </summary>
+    [Test]
+    public void Class_Has_NetDaemonAppAttribute()
+    {
+        Attribute.GetCustomAttribute(typeof(T), typeof(NetDaemonAppAttribute))
+            .Should().NotBeNull();
+    }
+
+    /// <summary>
+    /// Create a new Instance of the App/System Under Test (SUT)
+    /// </summary>
+    /// <param name="hass"></param>
+    /// <returns></returns>
+    protected abstract T CreateSUT(IHaContext hass);
+
+    /// <summary>
+    /// Create a Fake HaContext Instance
+    /// </summary>
+    /// <returns></returns>
+    protected Mock<IHaContext> FakeHass() => new();
+}
---
+++ NetDaemonApps/Dtos/HaNotify.cs
@@ -0,0 +1,9 @@
+﻿namespace NetDaemonApps.Dtos;
+
+public class HaNotify(string _title, string _message)
+{
+    public static string Domain => "notify";
+    public static string Service => "persistent_notification";
+    public string title => _title;
+    public string message => _message;
+}
