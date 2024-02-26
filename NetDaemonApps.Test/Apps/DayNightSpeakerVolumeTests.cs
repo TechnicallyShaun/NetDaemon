f5ca5f530d2b2bda4eeb027350e6bc9efcf1f0d6
Smithy <121695494+ssmithy@users.noreply.github.com>
2024-02-26 21:07:51 +0000

    set volume to a schedule

--- NetDaemonApps.Test/Apps/DayNightSpeakerVolumeTests.cs
+++ NetDaemonApps.Test/Apps/DayNightSpeakerVolumeTests.cs
@@ -1,5 +1,50 @@
-﻿namespace NetDaemonApps.Test.Apps;
+﻿using NetDaemon.AppModel;
 
-public class DayNightSpeakerVolumeTests : AppTestsBase<DayNightSpeakerVolume>
+namespace NetDaemonApps.Test.Apps;
+
+public class DayNightSpeakerVolumeTests : AppTestsBase<DayNightSpeakerVolumeApp>
 {
+    //--------------------------------------------------
+    // IMPROVEMENT VOLUME PROFILE -> DEVICE SETTER
+    //--------------------------------------------------
+    // LIST OF SPEAKERS
+    // LIST OF TIMES AND VOLUMES
+    // POSSIBLE DAY OF WEEK / RANGE
+    // ENFORCE MODE (where if volume is changed, it is corrected)
+
+    private IAppConfig<DayNightSpeakerVolume> CreateConfig()
+    {
+        var config = Mock.Of<IAppConfig<DayNightSpeakerVolume>>();
+
+        config.Value.DayStart = "7.30";
+        config.Value.DayVolume = 9;
+
+        config.Value.NightStart = "19.30";
+        config.Value.NightVolume = 3;
+
+        return config;
+    }
+
+    // Tests
+    //  - Day Event is set
+    //  - Night event is set
+    //  - Volume Set is called (with correct volume level, for day and night)
+
+
+    [Test]
+    public void TestCron()
+    {
+        var haContextMoq = new Mock<IHaContext>();
+        var fakeConfig = CreateConfig();
+
+        var testScheduler = new Microsoft.Reactive.Testing.TestScheduler();
+        testScheduler.AdvanceTo(new DateTime(2020, 2, 1, 23, 44, 0).ToUniversalTime().Ticks);
+
+        var app = new DayNightSpeakerVolumeApp(haContextMoq.Object, testScheduler, fakeConfig);
+
+        haContextMoq.VerifyNoOtherCalls();
+        testScheduler.AdvanceBy(TimeSpan.FromMinutes(1).Ticks);
+
+
+    }
 }
---
+++ NetDaemonApps/Apps/DayNightSpeakerVolumeApp.cs
@@ -0,0 +1,35 @@
+﻿using NetDaemon.Extensions.Scheduler;
+using NetDaemon.HassModel;
+using NetDaemon.HassModel.Entities;
+using NetDaemonApps.Dtos;
+using System.Linq;
+using System.Reactive.Concurrency;
+
+namespace NetDaemonApps.Apps;
+
+[NetDaemonApp]
+public class DayNightSpeakerVolumeApp
+{
+    private DayNightSpeakerVolume config;
+
+    public DayNightSpeakerVolumeApp(IHaContext ha, IScheduler scheduler, IAppConfig<DayNightSpeakerVolume> config)
+    {
+        this.config = config.Value;
+
+        scheduler.ScheduleCron("0 7 * * *", () => {
+            ha.Entity("media_player.living_room_speaker").CallService("media_player.volume_set", new { volume_level = 0.9 });
+        });
+
+        scheduler.ScheduleCron("0 21 * * *", () => {
+            ha.Entity("media_player.living_room_speaker").CallService("media_player.volume_set", new { volume_level = 0.3 });
+        });
+
+    }
+}
+public class DayNightSpeakerVolume
+{
+    public string? DayStart { get; set; }
+    public decimal? DayVolume { get; set; }
+    public string? NightStart { get; set; }
+    public decimal? NightVolume { get; set; }
+}
